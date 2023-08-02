384fe3e5cce9b5ef1e9efd4960c4f42b99a5a357
Cow-Kite <bbsr0819@gmail.com>
2023-08-02 20:25:36 +0900

    graph_clustering Update

--- Horovod/GCN/graph_clustering.py
+++ Horovod/GCN/graph_clustering.py
@@ -3,13 +3,21 @@ from torch_geometric.datasets import Planetoid
 import torch_geometric.loader as loader
 import torch_geometric.transforms as T
 
+import horovod
+import horovod.torch as hvd
+
+hvd.init()
+print(hvd.size())
+
 dataset = Planetoid(root='./data', name='Cora')
 graph = dataset[0]
 split = T.RandomNodeSplit(num_val=0.1, num_test=0.2)
 graph = split(graph)
 
-cluster = loader.ClusterData(graph, 8)
+cluster = loader.ClusterData(graph, num_parts=hvd.size())
 clusterloader = loader.ClusterLoader(cluster)
+
 
 for i in clusterloader:
     print(i)
+    
